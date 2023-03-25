6a78129bf71abe7846a1a488a9fb00b705302035
hopeAlexis <115659713+hopeAlexis@users.noreply.github.com>
2023-03-25 12:29:14 +0200

    Add files via upload

---
+++ aaaaaaaaaaaaaaaaaaaa.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+class Cipher
+{
+private:
+	int c_fibonacci[30] = { 1,2 };
+	int c_wordAmount = 0;
+	std::string c_message = "";
+	std::string c_words[30];
+
+	void fiboFinder()
+	{
+		for (size_t i = 2; i <= c_wordAmount; i++)
+		{
+			c_fibonacci[i] = c_fibonacci[i - 1] + c_fibonacci[i - 2];
+		}
+	}
+
+	void wordCount(const std::string& message)
+	{
+		for (size_t i = 0; i < message.length(); i++)
+		{
+			if (message[i] == ' ')
+			{
+				c_wordAmount++;
+
+			}
+		}
+	}
+
+
+public:
+	Cipher(std::string str)
+	{
+		c_message = str;
+		wordCount(str);
+		fiboFinder();
+
+		int count = 0;
+		int 
+
+		for (size_t i = 0; i < str.length(); i++)
+		{
+			if (str[i] == ' ')
+			{
+				c_words[count] = str.substr()
+			}
+		}
+	}
+};
+
+
+
+
+int main()
+{
+	std::string str;
+	std::getline(std::cin, str);
+}
