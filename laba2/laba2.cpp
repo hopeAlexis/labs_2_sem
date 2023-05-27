07a70418552badaacfb3a89ebfec16d9f4da4aff
hopeAlexis <dododor.mistt@gmail.com>
2023-05-27 13:40:57 +0200

    Update laba2.cpp

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -1,7 +1,8 @@
 //Лабораторная работа №2
 //«Самодельный string»
 //
-//Реализуйте класс String для работы со строками.Понятное дело, что std::string для реализации использовать нельзя, только char.
+//Реализуйте класс String для работы со строками.
+//Понятное дело, что std::string для реализации использовать нельзя, только char.
 //
 //У вас должны быть реализованы :
 //1.	Базовый конструктор
@@ -20,10 +21,68 @@
 //Продемонстрируйте работу вашего класса.Будьте готовы объяснить написанное.
 
 #include <iostream>
+
 class String
 {
 private:
-
+	size_t s_size = 0;
+	char* s_str = nullptr; //why * ?
 public:
+	friend std::ostream& operator << (std::ostream& out, const String& str); //output using a friend method
+	friend std::istream& operator >> (std::istream& in, String& str); //input
+
+	String(const char* str) : s_size(strlen(str)), s_str(new char[s_size + 1]) //base costructor
+	{
+		std::copy(str, str + s_size + 1, s_str); // +1 for terminating symbol 
+	}
+
+	String(char )
+
+	~String() //destructor
+	{
+		if (s_str != nullptr)
+			delete[] s_str;
+	}
+
+
 
 };
+
+std::ostream& operator << (std::ostream& out, const String & str)
+{
+	for (int i = 0; i < str.s_size; i++)
+	{
+		out << str.s_str[i];
+	}
+	return out;
+}
+
+std::istream& operator >> (std::istream& in, String& str)
+{
+	char* tmp = new char[10000];
+	in >> tmp;
+	int index = -1;
+	for (int i = 0; i < 10000; i++)
+	{
+		if (tmp[i] == '\0') //entering until a terminating symbol
+		{
+			index = i;
+			break;
+		}
+	}
+	str.s_size = index + 1;
+	delete[] str.s_str;
+	str.s_str = new char[str.s_size + 1];
+	std::copy(tmp, tmp + index + 1, str.s_str);
+	delete[] tmp;
+
+	return in;
+}
+
+int main()
+{
+
+
+	String string("Hello!");
+
+}
