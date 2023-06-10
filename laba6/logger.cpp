df5980c80fd21349f1b172b39bbbd1f1e4647511
hopeAlexis <dododor.mistt@gmail.com>
2023-06-10 03:37:16 +0200

    all labs

---
+++ laba6/logger.cpp
@@ -0,0 +1,81 @@
+﻿#include "Logger.hpp"
+
+namespace my {
+
+    Logger::Logger(const std::string& logFilePath) {
+        m_logFile.open(logFilePath);
+        m_logLevel = LogLevel::Info;    // default log level 
+    }
+
+    Logger::~Logger() {
+        if (m_logFile.is_open()) {
+            m_logFile.close();
+        }
+    }
+
+    void Logger::setLogLevel(LogLevel level) {
+        m_logLevel = level;
+    }
+
+    std::string Logger::getCurrentDateAndTime() const {
+        auto now = std::chrono::system_clock::now();
+        auto time = std::chrono::system_clock::to_time_t(now);
+
+        char buffer[20];    // 19 + '/0'
+        std::strftime(buffer, sizeof(buffer), "%d-%m-%Y %X", std::localtime(&time));        //%d-%m-%Y %X  -  day month year
+        return buffer;
+    }
+
+    // time + log level
+    std::string Logger::formatLogMessage(const std::string& message, LogLevel level) const {
+        std::string formattedMessage = getCurrentDateAndTime() + " ";
+
+        switch (level) {
+        case LogLevel::Info:
+            formattedMessage += "[INFO] ";
+            break;
+        case LogLevel::Debug:
+            formattedMessage += "[DEBUG] ";
+            break;
+        case LogLevel::Error:
+            formattedMessage += "[ERROR] ";
+            break;
+        default:
+            break;
+        }
+
+        formattedMessage += message;
+        return formattedMessage;
+    }
+
+    void Logger::logInfo(const std::string& message) {
+        if (m_logLevel >= LogLevel::Info) {
+            std::string formattedMessage = formatLogMessage(message, LogLevel::Info);
+            std::cout << formattedMessage << "\n";
+            if (m_logFile.is_open()) {
+                m_logFile << formattedMessage << "\n";
+            }
+        }
+    }
+
+    void Logger::logDebug(const std::string& message) {
+        if (m_logLevel >= LogLevel::Debug) {
+            std::string formattedMessage = formatLogMessage(message, LogLevel::Debug);
+            std::cout << formattedMessage << "\n";
+            if (m_logFile.is_open()) {
+                m_logFile << formattedMessage << "\n";
+            }
+        }
+    }
+
+    void Logger::logError(const std::string& message) {
+        if (m_logLevel >= LogLevel::Error) {
+            std::string formattedMessage = formatLogMessage(message, LogLevel::Error);
+            std::cerr << formattedMessage << "\n";
+            if (m_logFile.is_open()) {
+                m_logFile << formattedMessage << "\n";
+            }
+        }
+    }
+
+}
---
+++ laba6/logger.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#pragma warning(disable : 4996) //to allow use of the deprecated functions
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+namespace my {
+
+    enum class LogLevel {
+        None,
+        Info,
+        Debug,
+        Error
+    };
+
+    class Logger {
+    private:
+        std::ofstream m_logFile;
+        LogLevel m_logLevel;    // current log level
+
+        std::string getCurrentDateAndTime() const;
+        std::string formatLogMessage(const std::string& message, LogLevel level) const;
+
+    public:
+        Logger(const std::string& logFilePath);
+
+        ~Logger();
+
+        void setLogLevel(LogLevel level);
+        void logInfo(const std::string& message);
+        void logDebug(const std::string& message);
+        void logError(const std::string& message);
+    };
+
+}
